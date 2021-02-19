4b12b5fd0473bcecec2febe321f336eb09c1c9d4
chirag vaid <chiragvaid88@example.com>
2021-02-19 18:57:53 +0530

    reverse of array through recursion added

--- reverseOfArray.java
+++ reverseOfArray.java
@@ -1,4 +1,7 @@
 import java.util.*;
+// Reverse an array by swapping the first and last digit and then 
+// Incrementing start by +1
+// and decrementing end by -1
 class Reverse
 {
     // 10 , 5 , 6 , 2
@@ -19,15 +22,35 @@ class Reverse
     }
     
 }
-public class Main
+// To reverse an array using recursion
+class ReverseThroughRecursion
+{
+    public void reverse(int arr[],int start,int end)
+    {
+        if(start>=end)
+        {
+            return ;
+        }
+        else
+        {
+            int temp=arr[start];
+            arr[start] =arr[end];
+            arr[end] = temp;
+            reverse(arr, start+1, end-1);
+        }
+    }
+}
+class Main
 {
     public static void main(String args [])
     {
         Reverse ob = new Reverse();
-        int arr[] = {10,5,6,2};
-        int size = 4;
-        int returnedArray[] = ob.reverse(arr,size);
-        for(int i=0;i<4;i++)
+        ReverseThroughRecursion ob1 = new ReverseThroughRecursion();
+        int arr[] = {10,5,3,6,2};
+        int size = arr.length;
+        // int returnedArray[] = ob.reverse(arr,size);
+        ob1.reverse(arr,0,size-1);
+        for(int i=0;i<size;i++)
         {
             System.out.println(arr[i]);
         }
